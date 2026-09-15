a7d86cc7d1eb6ec0bf113a1ba9bba1d52c014452
Valentina Cecilia Romero Damián <romer@salsa.paco.ranccon>
2026-09-15 17:03:18 -0600

    Crear pagina inicial de ITT3 Tienda

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITT3 Tienda</title>
+</head>
+<body>
+
+    <h1>ITT3 Tienda</h1>
+    <p>Águilas Reales</p>
+    <p>Bienvenido a nuestra tienda oficial.</p>
+
+</body>
+</html>
